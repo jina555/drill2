65423eee0e340017556d2e7f11b09fec4fe93b7e
jina555 <kja390@tukorea.ac.kr>
2025-09-16 21:19:53 +0900

    Drill #2 제출-2023182040 김진아

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,57 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass= load_image('grass.png')
+character=load_image('character.png')
 
-close_canvas()
+while True:
+    x = 400
+    while (x <= 780):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 2
+        delay(0.01)
+
+    y = 90
+    while (y <= 580):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(780, y)
+        y = y + 2
+        delay(0.01)
+
+    x = 780
+    while (x <= 780 and x > 10):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 580)
+        x = x - 2
+        delay(0.01)
+
+    y = 580
+    while (y <= 580 and y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(10, y)
+        y = y - 2
+        delay(0.01)
+
+    x = 1
+    while (x <= 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 2
+        delay(0.01)
+
+    import math
+
+    theta = -3 / 2 * math.pi
+    r = 210
+    while (theta < 1 / 2 * math.pi):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(400 + r * math.cos(theta), 300 - r * math.sin(theta))
+        theta = theta + 0.01
+        delay(0.01)
